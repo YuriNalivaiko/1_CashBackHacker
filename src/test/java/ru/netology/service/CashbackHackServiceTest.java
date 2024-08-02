5a9a698993feae1450dc3f1ac013c5e4ec28dca0
“Yuri <nalivaikoyuri@gmail.com>
2024-08-02 13:18:21 +0300

    Added tests using JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,29 +1,35 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class CashbackHackServiceTest {
 
+    CashbackHackService cashBackHacker = new CashbackHackService();
+
     @Test
-    public void shouldReturnAmountToBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int expected = 100;
-
-        int actual = service.remain(amount);
-
-        Assert.assertEquals(actual, expected);
+    public void testRemainLessThanBoundary() {
+        int amount = 500;
+        int expected = 500;
+        int actual = cashBackHacker.remain(amount);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldReturnZeroIfAmountIsMultipleOfBoundary() {
-        CashbackHackService service = new CashbackHackService();
+    public void testRemainEqualToBoundary() {
         int amount = 1000;
         int expected = 0;
+        int actual = cashBackHacker.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
 
-        int actual = service.remain(amount);
-
-        Assert.assertEquals(actual, expected);
+    @Test
+    public void testRemainGreaterThanBoundary() {
+        int amount = 1500;
+        int expected = 500;
+        int actual = cashBackHacker.remain(amount);
+        Assert.assertEquals(expected, actual);
     }
 }
